0dda540d15342bce2482a54bfceaa8ca184a9c25
hkoo <tyrfingr12@naver.com>
2020-01-14 02:58:57 +0900

    현재 슬랙 환경 적용 완료 메소드 변경 적용중

--- src/main/java/com/hkoo/slackbot/SlackbotApplication.java
+++ src/main/java/com/hkoo/slackbot/SlackbotApplication.java
@@ -2,12 +2,18 @@ package com.hkoo.slackbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.hkoo.slackbot"})
 public class SlackbotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SlackbotApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
